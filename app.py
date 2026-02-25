99920a943a7c2e1b760d4d1730215d608a389073
Tejaswi Anand Khare <83838884+Tejaswi-Anand-Khare@users.noreply.github.com>
2026-02-25 10:53:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -60,7 +60,7 @@ def save_message(role, content):
 def ask_llm(prompt):
 
     return client.chat.completions.create(
-        model="llama3-8b-8192",
+        model="llama-3.1-8b-instant",
         messages=[
             {"role": "system",
              "content": "You are Atlas, a helpful AI assistant."},
